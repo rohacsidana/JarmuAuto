29df4ce9eb9fba9c7632eb31a4c2081b440de5ab
rohacsidana <rohacsi.dana@gmail.com>
2023-02-13 14:40:34 +0100

    metódusok specifikáció szerint

--- src/model/Auto.java
+++ src/model/Auto.java
@@ -1,16 +1,24 @@
 package model;
 
-public class Auto extends Jarmu{
+import java.util.Random;
+
+public class Auto extends Jarmu {
 
     private boolean defekt;
 
     public void kereketCserel() {
+        this.defekt = false;
     }
 
     @Override
     public boolean halad() {
+        Random rnd = new Random();
+        int defektesLesz = rnd.nextInt(4 - 1) + 1;
+        if (defektesLesz == 4) {
+            this.defekt = true;
+            this.kereketCserel(); // mindenképp cseréljünk?
+        }
         return true;
     }
-    
-    
+
 }
--- src/model/Jarmu.java
+++ src/model/Jarmu.java
@@ -2,21 +2,36 @@ package model;
 
 public abstract class Jarmu {
 
-    private boolean beinditva;
-    private boolean uzemanyag;
-    private boolean megekerkezett;
+    private boolean beinditva, uzemanyag, megekerkezett;
+
+    public Jarmu() {
+        this.uzemanyag = true;
+        this.beinditva = false;
+        this.megekerkezett = false;
+    }
 
     public void beindit() {
+        this.beinditva = true;
     }
 
     public void leallit() {
+        this.beinditva = false;
     }
 
     public boolean tankol() {
+        if (this.beinditva) {
+            this.leallit();
+        }
+        this.uzemanyag = true;
         return true;
     }
 
     public boolean halad() {
+        //érkezzen meg?
+        if (this.megekerkezett) {
+            this.uzemanyag = false;
+            this.leallit();
+        }
         return true;
     }
 }
