3717f233d262d7a8706a2fe47734d743fb70bc43
rohacsidana <rohacsi.dana@gmail.com>
2023-02-13 13:49:24 +0100

    osztalyok, metodusok

---
+++ src/model/Auto.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Auto extends Jarmu{
+
+    private boolean defekt;
+
+    public void kereketCserel() {
+    }
+
+    @Override
+    public boolean halad() {
+        return true;
+    }
+    
+    
+}
---
+++ src/model/Jarmu.java
@@ -0,0 +1,22 @@
+package model;
+
+public abstract class Jarmu {
+
+    private boolean beinditva;
+    private boolean uzemanyag;
+    private boolean megekerkezett;
+
+    public void beindit() {
+    }
+
+    public void leallit() {
+    }
+
+    public boolean tankol() {
+        return true;
+    }
+
+    public boolean halad() {
+        return true;
+    }
+}
